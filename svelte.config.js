5be9e4e4117bce97c9c1fcdb868b3bf71c2233a2
arthur <arthur.broudoux@gmail.com>
2024-12-16 15:03:30 +0100

    fix: configure appDir Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -11,13 +11,14 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		appDir: "app", // Required as the default is _app
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
 			fallback: null
 		}),
 		paths: {
-			base: "/dtools"
+			base: "/dtools/index.html"
 		}
 	},
 	extensions: [".svelte", ".svx", ".md"]
