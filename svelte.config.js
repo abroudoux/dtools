199ee814a8fc2243b5ab70ba0895f8cd22bb366a
arthur <arthur.broudoux@gmail.com>
2024-12-16 15:40:31 +0100

    feat: create github action Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- svelte.config.js
+++ svelte.config.js
@@ -11,12 +11,12 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		appDir: "app", // Required as the default is _app
 		adapter: adapter({
 			pages: "build",
 			assets: "build",
-			fallback: null,
-			strict: true
+			fallback: undefined,
+			strict: true,
+			precompress: false
 		}),
 		paths: {
 			base: "/dtools"
